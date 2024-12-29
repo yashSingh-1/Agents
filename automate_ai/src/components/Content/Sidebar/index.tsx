810c151d19e2d9dbfa8f1f2e9d0dc7a41a4045df
Yash Singh <dobbyneedsclothes@gmail.com>
2024-12-29 22:37:02 +0530

    streak

--- automate_ai/src/components/Content/Sidebar/index.tsx
+++ automate_ai/src/components/Content/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { usePaths } from "@/hooks/use-nav";
+import Logo from "../Logo";
 
 type Props = {
   slug: string;
@@ -9,11 +10,25 @@ const Sidebar = ({ slug }: Props) => {
   const { page, pathname } = usePaths();
   return (
     <div
-      className="w-[250px] h-screen border-2 radial fixed left-0 lg:inline-block
-    border-[#545454] bg-gradient-to-b from-[#7688DD] via-[#171717]
-    to-[#7688DD] hidden botoom-0 top-0 m-3 rouded-3xl overflow-hidden"
+      className="w-[250px] 
+    border-[1px]
+    radial 
+    fixed 
+    left-0 
+    lg:inline-block
+    border-[#545454] 
+    bg-gradient-to-b from-[#768BDD] 
+    via-[#171717]
+     to-[#768BDD] 
+     hidden 
+     bottom-0 
+     top-0 
+     m-3 
+     rounded-3xl 
+     overflow-hidden"
     >
-      <div className="flex flex-col 
+      <div
+        className="flex flex-col 
       gap-y-5
        w-full 
        h-full 
@@ -23,9 +38,13 @@ const Sidebar = ({ slug }: Props) => {
        bg-clip-padding 
        backdrop-filter 
        backdrop--blur__safari 
-       backdrop-blur-3xl">
+       backdrop-blur-3xl"
+      >
         <div className="flex gap-x-2 items-center p-5 justify-center">
-          {/* <LogoSmall /> */}
+          <Logo />
+        </div>
+        <div className="flex flex-col py-3">
+          something
         </div>
       </div>
     </div>
