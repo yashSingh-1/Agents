a0e5951a30f594c1a4e0656fc16fc7e84e4b503d
Yash Singh <dobbyneedsclothes@gmail.com>
2025-02-08 17:59:37 +0530

    Added .gitignore and automate_ai directory

---
+++ automate_ai/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/yashsingh/Documents/Agents/automate_ai/src/app/layout\":[\"static/media/2b3f1035ed87a788-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
