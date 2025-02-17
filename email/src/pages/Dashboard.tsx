37563328ebcec58d5f16bad4617e743854706030
Yash Singh <dobbyneedsclothes@gmail.com>
2025-02-18 00:43:55 +0530

    libs setting up

---
+++ email/src/pages/Dashboard.tsx
@@ -0,0 +1,18 @@
+
+const Dashboard = () => {
+  return (
+    <div className="flex justify-start">
+        <div className="w-1/3">
+            Sidebar
+        </div>
+        <div className="w-1/3">
+            Maincomponent
+        </div>
+        <div className="w-1/3">
+            Email
+        </div>
+    </div>
+  )
+}
+
+export default Dashboard
